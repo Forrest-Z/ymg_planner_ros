fccebb96d0d94ef2b8967b2ffa37776fbeca5779
strymj <yamaji.shoutarou@gmail.com>
2017-08-28 16:40:01 +0900

    update

--- ymg_local_planner/src/map_grid_cost_function_kai.cpp
+++ ymg_local_planner/src/map_grid_cost_function_kai.cpp
@@ -40,28 +40,39 @@ double MapGridCostFunctionKai::getCellCosts (unsigned int px, unsigned int py)
 double MapGridCostFunctionKai::scoreTrajectory(Trajectory &traj)
 {/*{{{*/
   double cost = 0.0;
-  double foot_x, foot_y, foot_th,  head_x, head_y;
-  unsigned int foot_cell_x, foot_cell_y,  head_cell_x, head_cell_y;
-  double foot_grid_dist, head_grid_dist;
+  double foot_x, foot_y, foot_th;
+  unsigned int foot_cell_x, foot_cell_y;
+  double foot_grid_dist;
 
 	int score_traj_index = (traj.getPointsSize()-1) * valid_traj_ratio_; 
+	std::cout<<"traj_size - score_traj_index"<<traj.getPointsSize()<<" - "<<score_traj_index<<std::endl;
 	traj.getPoint(score_traj_index, foot_x, foot_y, foot_th);
-
-	head_x = foot_x + forward_point_distance_ * cos(foot_th);
-	head_y = foot_y + forward_point_distance_ * sin(foot_th);
 
 	if ( ! costmap_->worldToMap(foot_x, foot_y, foot_cell_x, foot_cell_y)) {
 		ROS_WARN("Off Map (foot) %f, %f", foot_x, foot_y);
 		return -5.0;
 	}
-
-	if ( ! costmap_->worldToMap(head_x, head_y, head_cell_x, head_cell_y)) {
-		ROS_WARN("Off Map (head) %f, %f", head_x, head_y);
-		return -4.0;
-	}
-
+	
 	foot_grid_dist = getCellCosts(foot_cell_x, foot_cell_y);
-	head_grid_dist = getCellCosts(head_cell_x, head_cell_y);
+	cost = foot_grid_dist;
+
+	// if forward_point_distance set positive, calc heading score
+	if (0.0 < forward_point_distance_) {
+		double head_x, head_y;
+		unsigned int head_cell_x, head_cell_y;
+		double head_grid_dist;
+
+		head_x = foot_x + forward_point_distance_ * cos(foot_th);
+		head_y = foot_y + forward_point_distance_ * sin(foot_th);
+
+		if ( ! costmap_->worldToMap(head_x, head_y, head_cell_x, head_cell_y)) {
+			ROS_WARN("Off Map (head) %f, %f", head_x, head_y);
+			return -4.0;
+		}
+
+		head_grid_dist = getCellCosts(head_cell_x, head_cell_y);
+		cost = (foot_grid_dist + head_grid_dist) / 2.0;
+	}
 
 	//if a point on this trajectory has no clear path to the goal... it may be invalid
 	if (stop_on_failure_) {
@@ -72,7 +83,6 @@ double MapGridCostFunctionKai::scoreTrajectory(Trajectory &traj)
 		}
 	}
 
-	cost = (foot_grid_dist + head_grid_dist) / 2.0;
 
   return cost;
 }/*}}}*/
