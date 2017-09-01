8096c0a7f89e434cc824753c5521cc61611ddbb8
strymj <yamaji.shoutarou@gmail.com>
2017-09-01 15:16:03 +0900

    update

--- ymg_local_planner/src/ymglp.cpp
+++ ymg_local_planner/src/ymglp.cpp
@@ -225,7 +225,7 @@ namespace ymglp {
 		double now_distance = 0.0;
 		int local_goal_index = -1;
 		// for (int i=closest_index+1; i<global_plan_.size(); ++i) {
-		for (int i=0; i<global_plan_.size(); ++i) {
+		for (int i=1; i<global_plan_.size(); ++i) {
 			now_distance += sqDistance(global_plan_[i-1], global_plan_[i]);
 			if (local_goal_distance_ < now_distance) {
 				local_goal_index = i;
