80379e518719c54f6cc31a7a7193bbdad8b1ac14
strymj <yamaji.shoutarou@gmail.com>
2017-10-12 23:32:40 +0900

    update

--- ymggp_bgp/src/ymggp_bgp.cpp
+++ ymggp_bgp/src/ymggp_bgp.cpp
@@ -218,17 +218,21 @@ bool YmgGPBGP::makePlan(const geometry_msgs::PoseStamped& start, const geometry_
 		ROS_ERROR("This planner has not been initialized yet, but it is being used, please call initialize() before use");
 		return false;
 	}
+	ROS_INFO("RUNNING");
 
 	makeYmggpPlan(start, goal, plan);
 	publishYmggpPlan(plan);
+	ROS_INFO("RUNNING");
 
 	updateRobotStatus(start, goal, plan);
+	ROS_INFO("RUNNING");
 
 	// if the robot is near the BGP goal. changes algorithm to BGP.
 	if (use_bgp_ && ymglp::UtilFcn::calcDist(start, bgp_goal_) < recovery_dist_) {
 		ROS_INFO("[YmgGPHybROS] Changes planner to ymggp.");
 		setBGPFlag(false);
 	}
+	ROS_INFO("RUNNING");
 
 	if (use_bgp_) {
 		// ROS_INFO("path size: %d", (int)plan.size());
