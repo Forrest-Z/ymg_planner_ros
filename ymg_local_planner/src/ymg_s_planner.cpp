4c307c0966ef447d5a62563defb0ca8d0f0a7cca
strymj <yamaji.shoutarou@gmail.com>
2017-10-18 15:17:00 +0900

    update

--- ymg_local_planner/src/ymg_s_planner.cpp
+++ ymg_local_planner/src/ymg_s_planner.cpp
@@ -138,6 +138,7 @@ double YmgSPlanner::getTragetCurvature()
 	double r_center_y = (goal_r[0]*goal_r[0] - goal_r[1]*goal_r[1]) / (2*goal_r[1]);
 	double theta = atan2(r_center_y - goal_r[1], fabs(goal_r[0]));
 
+	ROS_INFO("goal : %f, %f", goal[0], goal[1]);
 	ROS_INFO("goal_r : %f, %f", goal_r[0], goal_r[1]);
 	ROS_INFO("r_center_y : %f", r_center_y);
 
